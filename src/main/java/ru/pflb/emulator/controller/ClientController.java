bd056e95804d0dc760bd2b02278927397685169e
Denis <akmatalievdns@gmail.com>
2020-09-02 21:38:52 +0300

    added rest methods

--- src/main/java/ru/pflb/emulator/controller/ClientController.java
+++ src/main/java/ru/pflb/emulator/controller/ClientController.java
@@ -1,6 +1,8 @@
 package ru.pflb.emulator.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.pflb.emulator.model.dto.ClientDto;
 import ru.pflb.emulator.service.ClientService;
@@ -12,26 +14,33 @@ import java.util.Random;
 @RestController
 public class ClientController {
 
-    private ClientService clientService;
+	private ClientService clientService;
 
-    public ClientController(ClientService clientService) {
-        this.clientService = clientService;
-    }
+	public ClientController(ClientService clientService) {
+		this.clientService = clientService;
+	}
 
-    @GetMapping("/client/{id}")
-    public ResponseEntity<ClientDto> getClient(@PathVariable long id) {
-        ClientDto client = clientService.getUserById(id);
-        return ResponseEntity.ok(client);
-    }
+	@GetMapping("/client/{id}")
+	public ResponseEntity<ClientDto> getClient(@PathVariable long id) {
+		ClientDto client = clientService.getUserById(id);
+		return ResponseEntity.ok(client);
+	}
 
-    @PostMapping("/addClient/{id}")
-    public ResponseEntity addClient(
-            @RequestBody ClientDto client) throws URISyntaxException {
+	@PostMapping("/client")
+	public ResponseEntity<ClientDto> addClient(@Validated
+			@RequestBody ClientDto client) {
+		return ResponseEntity.status(201).build();
+	}
 
-        return ResponseEntity.created(new URI("http://localhost:8080/client/{id}")).build();
-    }
+	@DeleteMapping("/client/{id}")
+	public ResponseEntity<ClientDto> deleteClient(
+			@PathVariable long id) {
+		return ResponseEntity.status(200).build();
+	}
 
-
-
-
+	@PutMapping("/client/{id}")
+	public ResponseEntity<ClientDto> updateClient(@RequestBody ClientDto client,
+			@PathVariable long id) {
+		return new ResponseEntity<>(client, HttpStatus.OK);
+	}
 }
